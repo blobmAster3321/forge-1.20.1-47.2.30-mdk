b5801b7c9ed1c27d83a681ba2b988e87c83b0330
blobmAster3321 <blobmAster3321@gmail.com>
2024-05-09 17:20:25 -0400

    hetrick

--- src/main/java/net/blobmAster/testmod/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/blobmAster/testmod/datagen/loot/ModBlockLootTables.java
@@ -19,7 +19,7 @@ import net.minecraftforge.registries.RegistryObject;
 
 import java.util.Set;
 public class ModBlockLootTables extends BlockLootSubProvider{
-    protected ModBlockLootTables(Set<Item> pExplosionResistant, FeatureFlagSet pEnabledFeatures) {
+    public ModBlockLootTables() {
         super(Set.of(), FeatureFlags.REGISTRY.allFlags());
     }
 
